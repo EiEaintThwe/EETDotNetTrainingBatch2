0920fdb8c5af37ca309d145396cd4bf6b9261b95
EiEaintThwe <eieaintthwe9898@gmail.com>
2025-08-31 19:27:11 +0630

    feat: saleFeatureService Execute

--- EETDotNetTraningBatch2.POSConsoleApp/Program.cs
+++ EETDotNetTraningBatch2.POSConsoleApp/Program.cs
@@ -1,56 +1,59 @@
 ﻿
 using EETDotNetTraningBatch2.POSConsoleApp;
 
-Console.WriteLine("Welcome, Mini POS!");
+//Console.WriteLine("Welcome, Mini POS!");
 
-Result:
-Console.WriteLine("Menus");
-Console.WriteLine("-----------------------------------");
-Console.WriteLine("1. Product");
-Console.WriteLine("2. Sale");
-Console.WriteLine("3. Sale Detail");
-Console.WriteLine("4. Exit");
+SaleFeatureService saleFeatureService = new SaleFeatureService();
+saleFeatureService.Execute();
 
-Console.WriteLine("-----------------------------------");
+//Result:
+//Console.WriteLine("Menus");
+//Console.WriteLine("-----------------------------------");
+//Console.WriteLine("1. Product");
+//Console.WriteLine("2. Sale");
+//Console.WriteLine("3. Sale Detail");
+//Console.WriteLine("4. Exit");
 
-Console.Write("Choose Menu : ");
-string result = Console.ReadLine()!;
-bool isInt = int.TryParse(result, out int no);
-if (!isInt)
-{
-    Console.WriteLine("Invalid Product Menu. Please choose 1 to 4");
-    goto Result;
-}
+//Console.WriteLine("-----------------------------------");
 
-EnumMenu menu = (EnumMenu)no;
-switch (menu)
-{
+//Console.Write("Choose Menu : ");
+//string result = Console.ReadLine()!;
+//bool isInt = int.TryParse(result, out int no);
+//if (!isInt)
+//{
+//    Console.WriteLine("Invalid Product Menu. Please choose 1 to 4");
+//    goto Result;
+//}
+
+//EnumMenu menu = (EnumMenu)no;
+//switch (menu)
+//{
   
-    case EnumMenu.Product:
-        ProductService productService = new ProductService();
-        productService.Execute();
-        goto Result;
-        break;
-    case EnumMenu.Sale:
-        SaleService saleService = new SaleService();
-        saleService.Execute();
-        goto Result;
-        break;
-    case EnumMenu.SaleDetail:
-        SaleDetailService saleDetailService = new SaleDetailService();
-        saleDetailService.Execute();
-        goto Result;
-        break;
-    case EnumMenu.Exit:
-        goto End;
-    case EnumMenu.None:
-    default:
-        Console.WriteLine("Invalid Menu. Please choose 1 to 4");
-        goto Result;
-}
+//    case EnumMenu.Product:
+//        ProductService productService = new ProductService();
+//        productService.Execute();
+//        goto Result;
+//        break;
+//    case EnumMenu.Sale:
+//        SaleService saleService = new SaleService();
+//        saleService.Execute();
+//        goto Result;
+//        break;
+//    case EnumMenu.SaleDetail:
+//        SaleDetailService saleDetailService = new SaleDetailService();
+//        saleDetailService.Execute();
+//        goto Result;
+//        break;
+//    case EnumMenu.Exit:
+//        goto End;
+//    case EnumMenu.None:
+//    default:
+//        Console.WriteLine("Invalid Menu. Please choose 1 to 4");
+//        goto Result;
+//}
 
-End:
-Console.WriteLine("Exiting Mini POS....");
+//End:
+//Console.WriteLine("Exiting Mini POS....");
 
 
 //ProductService productService = new ProductService();
